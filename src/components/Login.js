03e236688c5b9c81a0808a664f57a08a330931de
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-10-21 12:21:34 +0600

    add setting a firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import {Container, Grid} from '@mui/material'
+import {Button, Container, Grid} from '@mui/material'
+import { Box } from '@mui/system'
 
 function Login() {
   return (
 	 <Container>
-     <Grid container style={{heigh:window.innerHeight - 50}}  justifyContent={'center'} alignContent={'center'}>
-        sggdsgh
+     <Grid container style={{heigh:window.innerHeight - 50}} alignItems={'center'} justifyContent={'center'} >
+     <Grid style={{width:400, background:'lightgray'}} container alignItems={'center'} direction={'column'}>
+     <Box p={5}>
+          <Button variant={'outlined'} >
+            Войти с помощью Google
+          </Button>
+        </Box>
+     </Grid>
+       
      </Grid>
    </Container>
   )
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyBtnM4ccCYlNgkay6vfRW6UboSZdqoSLio",
+  authDomain: "chat-react-d2ab5.firebaseapp.com",
+  projectId: "chat-react-d2ab5",
+  storageBucket: "chat-react-d2ab5.appspot.com",
+  messagingSenderId: "111888870969",
+  appId: "1:111888870969:web:bb0217c694c8823560e7e8",
+  measurementId: "G-65R8BJ0KNE"
+};
 root.render(
   <React.StrictMode>
     <App />
